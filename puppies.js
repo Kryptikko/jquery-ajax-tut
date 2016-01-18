46aa6aa68bd1cd6a7a67620f0564e0a61001a214
Kryptikko <velin.br.vangelov@gmail.com>
2016-01-18 14:20:37 +0200

    simplified example

--- puppies.js
+++ puppies.js
@@ -1,34 +1,45 @@
 $(document).ready(function(){
-			$.get('puppies.mst', function (template) {
-				Mustache.parse(template);
-
-				$.ajax({
-					url: 'puppies.json',
-					dataType: 'json',
-					complete: function (data) {
-						notie.alert(1, 'PUPPIES!!', 1.5);
-						data.responseJSON.forEach(function(item){
-							var rendered = Mustache.render(template, item);
-							$(rendered)
-								.data(item)
-								.appendTo($("#image-grid")).click(function(){
-									var name = $(this).data('name') || "Puppy";
-									var bark = $(this).data('bark') || "Baw Baw";
-									notie.alert(4, name + ' says: "' + bark + '"', 1.5);
-								});
-							//if(Math.random() >= 0.5)
-							//	item.addClass('image-grid-item-width2')
-						});
-						$("#image-grid").masonry({
-							itemSelector: '.image-grid-item',
-							gutter: 10,//margin between images
-							columnWidth: 200
-						});
-					},
-					failure: function (error) {
-						notie.error('no puppies');
-					}
-				});
+	$.ajax({
+		url: 'puppies.json',
+		dataType: 'json',
+		complete: function (data) {
+			notie.alert(1, 'PUPPIES!!', 1.5); //izkarva saobshtenie "puppies"
+			data.responseJSON.forEach(function(item){
+				var rendered = '<div class="image-grid-item">' +
+									'<header>' +
+										'<h3 class="puppy-name">' + item.name + '</h3>' +
+										'<p class="puppy-contnet">' + item.content + '</p>' +
+									'</header>' +
+									'<img src="' + item.image + '" />' +
+								'</div>';
+				$(rendered) //ot texta syzdawa dom nodeove
+					.data(item) // kym dom nodovete zakacha vsi4kata informaciq za kuchenceto za da moje da q polzwame po kusno
+					.appendTo($("#image-grid")) //pribavq dom node-a kym image-grid containera
+					.click(function(){ // dobavqme funkcionalnost na klik
+						// izpozlwame informaciqta ot data() metoda (vish 16ti red)
+						// $(this) ni dobawq jQuery funcionalnosta kum don node-a
+						// .data('key') proverqwa ima li stoinost kym 'key' (primerno item.key)
+						var name = $(this).data('name') || "Puppy";
+						var bark = $(this).data('bark');
+						// || "Puppy" se rawnqwa na 3te reda po nadoly (proverqwat dali bark ima stoinost ako ne my zadawa defaultna
+						if(!bark) {
+							bark = "Baw Baw";
+						}
+						//alertva usera che kuchenceto djavka
+						notie.alert(4, name + ' says: "' + bark + '"', 1.5);
+					});
 			});
-		});
+
+			//narejda kartnikite, mojesh da go mahnesh za da se zarejdat 1 pod druga
+			$("#image-grid").masonry({
+				itemSelector: '.image-grid-item',
+				gutter: 10,//margin between images
+				columnWidth: 200
+			});
+		},
+		failure: function (error) {
+			notie.error('no puppies');
+		}
+	});
+});
 
